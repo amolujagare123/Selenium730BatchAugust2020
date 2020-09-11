4c79e96e1f0148b10c78d4590735a4e5592c0f37
PC <amolujagare@gmail.com>
2020-09-11 20:37:27 +0530

    xpath

---
+++ src/LogindemoXpath.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LogindemoXpath {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("http://stock.scriptinglogic.net/");
+
+        WebElement txtUsername = driver.findElement(By.xpath("//input[@id='login-username']"));
+        // now lets type into the textbox ----> use sendKeys("text")
+        txtUsername.sendKeys("admin");
+
+        WebElement txtPassword = driver.findElement(By.xpath("//input[@type='password']"));
+        txtPassword.sendKeys("admin");
+
+        WebElement btnSubmit = driver.findElement(By.xpath("//input[@type='submit']"));
+        btnSubmit.click();
+
+    }
+}
