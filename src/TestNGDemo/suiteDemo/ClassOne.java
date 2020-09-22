cdbf88ae15a797c58a20059284f7754d6da2e4be
PC <amolujagare@gmail.com>
2020-09-22 20:54:57 +0530

    include exclude groups

--- src/TestNGDemo/suiteDemo/ClassOne.java
+++ src/TestNGDemo/suiteDemo/ClassOne.java
@@ -1,8 +1,23 @@
 package TestNGDemo.suiteDemo;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class ClassOne {
+
+   /* @BeforeTest
+    public void mymethod1()
+    {
+        System.out.println("@BeforeTest - mymethod1");
+    }
+
+
+    @AfterTest
+    public void mymethod2()
+    {
+        System.out.println("@AfterTest - mymethod2");
+    }*/
 
     @Test
     public void classOneTest1()
@@ -10,7 +25,7 @@ public class ClassOne {
         System.out.println("classOneTest1");
     }
 
-    @Test
+    @Test (groups = "email") // (enabled=false)
     public void classOneTest2()
     {
         System.out.println("classOneTest2");
--- src/TestNGDemo/suiteDemo/ClassThree.java
+++ src/TestNGDemo/suiteDemo/ClassThree.java
@@ -1,16 +1,33 @@
 package TestNGDemo.suiteDemo;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class ClassThree {
+
+     @BeforeSuite
+    public void mymethod1()
+    {
+        System.out.println("@BeforeSuite - mymethod1");
+    }
+
+
+    @AfterSuite
+    public void mymethod2()
+    {
+        System.out.println("@AfterSuite - mymethod2");
+    }
+
+
     @Test
     public void classThreeTest1()
     {
         System.out.println("classThreeTest1");
     }
 
-    @Test
-    public void classTwoTest2()
+    @Test (groups = "email")
+    public void classThreeTest2()
     {
         System.out.println("classThreeTest2");
     }
